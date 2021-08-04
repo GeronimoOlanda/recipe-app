397972fe90c87fd0ad1682d37d7222cc6e61a324
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-08-03 22:05:18 -0300

    Projeto receita

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './components/Recipes';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = '47e79328'; // ID para acesso
+  const APP_KEY = '44375f42db069967095b6857ce6390b7'; // Chave para acesso da aplicacao
+
+  const [recipes, setRecipes] = useState([]); // setando o estado vazio
+  const [search, setSearch] = useState(''); // setando o estado vazio
+  const [query, setQuery] = useState('chicken'); // setando o estado padrao como chicken
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+  // fazendo a requisicao para a API e retornando um arquivo json-
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  // ira atualizar sempre que a gente digitar uma letra
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // para solucionar o problema do updateSearch, se criou a setQuery e aqui esta passando ela como padrao
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <label>Pesquisar</label>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {/* quando formos retornar  um array com elementos html, usando o () */}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Recipes/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types'; // ES6
+import style from '../../recipe.module.css';
+import './index.css';
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className={style.container}>
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.title}> {ingredient.text} </li>
+        ))}
+      </ol>
+      <p>{calories}</p>
+      <img className={style.image} src={image} alt="" />
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  title: PropTypes.string,
+  calories: PropTypes.number,
+  image: PropTypes.string,
+  ingredients: PropTypes.string,
+};
+
+export default Recipe;
